dbe21dd2101cf793ed252fefd1b9d3119d80f917
MINGYEOL KIM <kmg1902@postech.ac.kr>
2022-07-27 23:52:41 +0900

    bottom lips simulated

--- src/index.js
+++ src/index.js
@@ -40,6 +40,14 @@ let RTy = document.getElementById("RTy");
 RTx.innerHTML = slider_RTx.value;
 RTy.innerHTML = slider_RTy.value;
 
+let slider_DL = document.getElementById("slider_DL");
+let DL = document.getElementById("DL");
+DL.innerHTML = slider_DL.value;
+
+let slider_DR = document.getElementById("slider_DR");
+let DR = document.getElementById("DR");
+DR.innerHTML = slider_DR.value;
+
 slider_LTx.oninput = function() {LTx.innerHTML = this.value;}
 slider_LTy.oninput = function() {LTy.innerHTML = this.value;}
 
@@ -52,7 +60,13 @@ slider_RBy.oninput = function() {RBy.innerHTML = this.value;}
 slider_RTx.oninput = function() {RTx.innerHTML = this.value;}
 slider_RTy.oninput = function() {RTy.innerHTML = this.value;}
 
+slider_DL.oninput = function() {DL.innerHTML = this.value;}
+slider_DR.oninput = function() {DR.innerHTML = this.value;}
+
 /* */
+
+const maxDL = 100;
+const maxDR = 100;
 
 // let state = {
 //   colors: [randomColor(), randomColor(), randomColor(), randomColor()],
@@ -120,19 +134,47 @@ function drawEyeLight(posX, posY) {
 function drawLips() {
     // additional conditions may be further needed (left < right)
     let offLT = [parseFloat(LTx.innerHTML), parseFloat(LTy.innerHTML)]; // [offsetX, offsetY]
-    let offLB = [parseFloat(LTx.innerHTML) < parseFloat(LBx.innerHTML) ? parseFloat(LBx.innerHTML) : parseFloat(LTx.innerHTML), parseFloat(LBy.innerHTML)];
-    let offRB = [parseFloat(RBx.innerHTML) > parseFloat(RTx.innerHTML) ? parseFloat(RTx.innerHTML) : parseFloat(RBx.innerHTML), parseFloat(RBy.innerHTML)];
+    let offLB = [parseFloat(LBx.innerHTML), parseFloat(LBy.innerHTML)];
+    let offRB = [parseFloat(RBx.innerHTML), parseFloat(RBy.innerHTML)];
     let offRT = [parseFloat(RTx.innerHTML), parseFloat(RTy.innerHTML)];
 
+    let LT_x_coord = (300 + offLT[0]) * ratio;
+    let LT_y_coord = (430 + offLT[1]) * ratio;
+    let LB_x_coord = (320 + offLB[0]) * ratio > LT_x_coord ? (320 + offLB[0]) * ratio : LT_x_coord;
+    let LB_y_coord = (450 + offLB[1]) * ratio;
+    let RT_x_coord = (500 + offRT[0]) * ratio;
+    let RT_y_coord = (430 + offRT[1]) * ratio;
+    let RB_x_coord = (480 + offRB[0]) * ratio < RT_x_coord ? (480 + offRB[0]) * ratio : RT_x_coord;
+    let RB_y_coord = (450 + offRB[1]) * ratio;
+
     ctx.beginPath();
-    ctx.moveTo((300 + offLT[0]) * ratio, (430 + offLT[1]) * ratio);
-    ctx.bezierCurveTo((320 + offLB[0]) * ratio, (500 + offLB[1]) * ratio, (480 + offRB[0]) * ratio, (500 + offRB[1]) * ratio, (500 + offRT[0]) * ratio, (430 + offRT[1]) * ratio);
+    ctx.moveTo(LT_x_coord, LT_y_coord);
+    ctx.bezierCurveTo(LB_x_coord, LB_y_coord, RB_x_coord, RB_y_coord, RT_x_coord, RT_y_coord);
     
     /* downside lips; positive value <= 100 */
-    let downL = 100;
-    let downR = 100;
-    ctx.moveTo((300 + offLT[0]) * ratio, (430 + offLT[1]) * ratio);
-    ctx.bezierCurveTo((300 + offLT[0]) * ratio, (500 + offLB[1] + downL) * ratio, (500 + offRT[0]) * ratio, (500 + offRB[1] + downR) * ratio, (500 + offRT[0]) * ratio, (430 + offRT[1]) * ratio);
+    let lenL = Math.sqrt(Math.pow(LB_x_coord - LT_x_coord, 2) + Math.pow(LB_y_coord - LT_y_coord, 2));
+    let lenR = Math.sqrt(Math.pow(RB_x_coord - RT_x_coord, 2) + Math.pow(RB_y_coord - RT_y_coord, 2));
+    
+    let downL = parseFloat(DL.innerHTML); // [0, maxDL]
+    let downR = parseFloat(DR.innerHTML); // [0, maxDR]
+
+    let thetaL0 = LB_y_coord === LT_y_coord ? Math.PI/2 : Math.atan((LB_x_coord - LT_x_coord)/(LB_y_coord - LT_y_coord)); // machine error may be further handled
+    let thetaL = thetaL0 < 0 ? Math.PI + thetaL0 : (thetaL0 === 0 && LB_y_coord < LT_y_coord ? Math.PI : thetaL0);
+    let Lsin = Math.sin(downL * Math.PI/(2 * maxDL));
+    let Lcos = Math.cos(downL * Math.PI/(2 * maxDL));
+    let dthetaL = thetaL * Lsin;
+
+    let thetaR0 = RB_y_coord === RT_y_coord ? Math.PI/2 : Math.atan((RT_x_coord - RB_x_coord)/(RB_y_coord - RT_y_coord)); // machine error may be further handled
+    let thetaR = thetaR0 < 0 ? Math.PI + thetaR0 : (thetaR0 === 0 && RB_y_coord < RT_y_coord ? Math.PI : thetaR0);
+    let Rsin = Math.sin(downR * Math.PI/(2 * maxDR));
+    let Rcos = Math.cos(downR * Math.PI/(2 * maxDR));
+    let dthetaR = thetaR * Rsin;
+
+
+    ctx.moveTo(LT_x_coord, LT_y_coord);
+    ctx.bezierCurveTo(LT_x_coord + ((lenL + downL)*Lcos + maxDL*Lsin) * Math.sin(thetaL - dthetaL), LT_y_coord + ((lenL + downL)*Lcos + maxDL*Lsin) * Math.cos(thetaL - dthetaL), RT_x_coord - ((lenR + downR)*Rcos + maxDR*Rsin) * Math.sin(thetaR - dthetaR), RT_y_coord + ((lenR + downR)*Rcos + maxDR*Rsin) * Math.cos(thetaR - dthetaR), RT_x_coord, RT_y_coord);
+    
+    // console.log(thetaL0);
     /* downside lips */
 
     // ctx.moveTo(300 * ratio, 430 * ratio);
