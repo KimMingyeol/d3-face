15bbcc19b023d18843538eaa96bad6c7966b4e45
MINGYEOL KIM <kmg1902@postech.ac.kr>
2022-07-28 23:45:01 +0900

    mouth disappeared; DEBUG in progress

--- src/index.js
+++ src/index.js
@@ -17,87 +17,22 @@ const eyebrow_width = 60 * ratio;
 
 document.body.appendChild(cnv)
 
-/* sliders to test lips' changes */
-let slider_LTx = document.getElementById("slider_LTx");
-let slider_LTy = document.getElementById("slider_LTy");
-let LTx = document.getElementById("LTx");
-let LTy = document.getElementById("LTy");
-LTx.innerHTML = slider_LTx.value;
-LTy.innerHTML = slider_LTy.value;
-
-let slider_LBdeg = document.getElementById("slider_LBdeg");
-let LBdeg = document.getElementById("LBdeg");
-LBdeg.innerHTML = slider_LBdeg.value;
-
-let slider_RBdeg = document.getElementById("slider_RBdeg");
-let RBdeg = document.getElementById("RBdeg");
-RBdeg.innerHTML = slider_RBdeg.value;
-
-let slider_RTx = document.getElementById("slider_RTx");
-let slider_RTy = document.getElementById("slider_RTy");
-let RTx = document.getElementById("RTx");
-let RTy = document.getElementById("RTy");
-RTx.innerHTML = slider_RTx.value;
-RTy.innerHTML = slider_RTy.value;
-
-let slider_DL = document.getElementById("slider_DL");
-let DL = document.getElementById("DL");
-DL.innerHTML = slider_DL.value;
-
-let slider_DR = document.getElementById("slider_DR");
-let DR = document.getElementById("DR");
-DR.innerHTML = slider_DR.value;
-
-slider_LTx.oninput = function() {LTx.innerHTML = this.value;}
-slider_LTy.oninput = function() {LTy.innerHTML = this.value;}
-
-slider_LBdeg.oninput = function() {LBdeg.innerHTML = this.value;}
-
-slider_RBdeg.oninput = function() {RBdeg.innerHTML = this.value;}
-
-slider_RTx.oninput = function() {RTx.innerHTML = this.value;}
-slider_RTy.oninput = function() {RTy.innerHTML = this.value;}
-
-slider_DL.oninput = function() {DL.innerHTML = this.value;}
-slider_DR.oninput = function() {DR.innerHTML = this.value;}
-/* */
-
-/* sliders to test eyebrows' changes */
-let slider_LTBrow = document.getElementById("slider_LTBrow");
-let slider_LBBrow = document.getElementById("slider_LBBrow");
-let LTBrow = document.getElementById("LTBrow");
-let LBBrow = document.getElementById("LBBrow");
-LTBrow.innerHTML = slider_LTBrow.value;
-LBBrow.innerHTML = slider_LBBrow.value;
-
-let slider_RTBrow = document.getElementById("slider_RTBrow");
-let slider_RBBrow = document.getElementById("slider_RBBrow");
-let RTBrow = document.getElementById("RTBrow");
-let RBBrow = document.getElementById("RBBrow");
-RTBrow.innerHTML = slider_RTBrow.value;
-RBBrow.innerHTML = slider_RBBrow.value;
-
-slider_LTBrow.oninput = function() {LTBrow.innerHTML = this.value;}
-slider_LBBrow.oninput = function() {LBBrow.innerHTML = this.value;}
-
-slider_RTBrow.oninput = function() {RTBrow.innerHTML = this.value;}
-slider_RBBrow.oninput = function() {RBBrow.innerHTML = this.value;}
-/* */
-
-/* sliders to test eyes' directions */
-let slider_EyeDist = document.getElementById("slider_EyeDist");
-let slider_EyeAngle = document.getElementById("slider_EyeAngle");
-let EyeDist = document.getElementById("EyeDist");
-let EyeAngle = document.getElementById("EyeAngle");
-EyeDist.innerHTML = slider_EyeDist.value;
-EyeAngle.innerHTML = slider_EyeAngle.value;
-
-slider_EyeDist.oninput = function() {EyeDist.innerHTML = this.value;}
-slider_EyeAngle.oninput = function() {EyeAngle.innerHTML = this.value;}
-/* */
-
 const maxDL = 100;
 const maxDR = 100;
+
+let face_state = {
+    LT: [0, 0], // [x, y]
+    RT: [0, 0], // [x, y]
+    LBdeg: 0,
+    RBdeg: 0,
+    DL: 0,
+    DR: 0,
+    LTBrow: 0,
+    RTBrow: 0,
+    LBBrow: 0,
+    RBBrow: 0,
+    EyeDir: [0, 0] // [EyeDist, EyeAngle]
+}
 
 const emotion_preset = {
     default: {
@@ -351,27 +286,6 @@ disgustButton.onclick = function() {
 }
 /* */
 
-// let state = {
-//   colors: [randomColor(), randomColor(), randomColor(), randomColor()],
-//   circle: {
-//     pos: {
-//       x: cnv.width / 4,
-//       y: cnv.height / 4,
-//     },
-//     radius: cnv.width / 4.5
-//   },
-//   square: {
-//     pos: {
-//       x: cnv.width / 2 + cnv.width / 6,
-//       y: 0 + cnv.height / 6,      
-//     },
-//     side: cnv.width / 2 - cnv.width / 3,
-//     angle: 0,
-//     color: randomColor()
-//   }
-// }
-
-
 setInterval(frame, 1000/20)
 
 function frame() {
@@ -384,8 +298,6 @@ function frame() {
     drawLeftEye();
     drawRightEye();
     drawLips();
-//   drawPie(state.colors.length)
-//   rotateSquareAndDrawSquare()
 }
 
 function emotion_init(etype) { // initiate changing face emotion
@@ -394,17 +306,17 @@ function emotion_init(etype) { // initiate changing face emotion
         change_state['t'] = 0;
         change_state['changing'] = true;
 
-        change_state['LT']['start'] = [parseFloat(LTx.innerHTML), parseFloat(LTy.innerHTML)];
-        change_state['RT']['start'] = [parseFloat(RTx.innerHTML), parseFloat(RTy.innerHTML)];
-        change_state['LBdeg']['start'] = parseFloat(LBdeg.innerHTML);
-        change_state['RBdeg']['start'] = parseFloat(RBdeg.innerHTML);
-        change_state['DL']['start'] = parseFloat(DL.innerHTML);
-        change_state['DR']['start'] = parseFloat(DR.innerHTML);
-        change_state['LTBrow']['start'] = parseFloat(LTBrow.innerHTML);
-        change_state['RTBrow']['start'] = parseFloat(RTBrow.innerHTML);
-        change_state['LBBrow']['start'] = parseFloat(LBBrow.innerHTML);
-        change_state['RBBrow']['start'] = parseFloat(RBBrow.innerHTML);
-        change_state['EyeDir']['start'] = [parseFloat(EyeDist.innerHTML), parseFloat(EyeAngle.innerHTML)];
+        change_state['LT']['start'] = [face_state['LT'][0], face_state['LT'][1]];
+        change_state['RT']['start'] = [face_state['RT'][0], face_state['RT'][1]];
+        change_state['LBdeg']['start'] = face_state['LBdeg'];
+        change_state['RBdeg']['start'] = face_state['RBdeg'];
+        change_state['DL']['start'] = face_state['DL'];
+        change_state['DR']['start'] = face_state['DR'];
+        change_state['LTBrow']['start'] = face_state['LTBrow'];
+        change_state['RTBrow']['start'] = face_state['RTBrow'];
+        change_state['LBBrow']['start'] = face_state['LBBrow'];
+        change_state['RBBrow']['start'] = face_state['RBBrow'];
+        change_state['EyeDir']['start'] = [face_state['EyeDir'][0], face_state['EyeDir'][1]];
     }
 }
 
@@ -421,29 +333,29 @@ function emotion_update() {
     let etype = change_state['emotion'];
     change_state['t'] += change_state['dt'];
 
-    LTx.innerHTML = String(change_state['LT']['start'][0] + (emotion_preset[etype]['LT'][0] - change_state['LT']['start'][0]) * Math.sin(Math.PI * change_state['t'] / 2));
-    LTy.innerHTML = String(change_state['LT']['start'][1] + (emotion_preset[etype]['LT'][1] - change_state['LT']['start'][1]) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['LT'][0] = String(change_state['LT']['start'][0] + (emotion_preset[etype]['LT'][0] - change_state['LT']['start'][0]) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['LT'][1] = String(change_state['LT']['start'][1] + (emotion_preset[etype]['LT'][1] - change_state['LT']['start'][1]) * Math.sin(Math.PI * change_state['t'] / 2));
 
-    RTx.innerHTML = String(change_state['RT']['start'][0] + (emotion_preset[etype]['RT'][0] - change_state['RT']['start'][0]) * Math.sin(Math.PI * change_state['t'] / 2));
-    RTy.innerHTML = String(change_state['RT']['start'][1] + (emotion_preset[etype]['RT'][1] - change_state['RT']['start'][1]) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['RT'][0] = String(change_state['RT']['start'][0] + (emotion_preset[etype]['RT'][0] - change_state['RT']['start'][0]) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['RT'][1] = String(change_state['RT']['start'][1] + (emotion_preset[etype]['RT'][1] - change_state['RT']['start'][1]) * Math.sin(Math.PI * change_state['t'] / 2));
 
-    LBdeg.innerHTML = String(change_state['LBdeg']['start'] + (emotion_preset[etype]['LBdeg'] - change_state['LBdeg']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
-    RBdeg.innerHTML = String(change_state['RBdeg']['start'] + (emotion_preset[etype]['RBdeg'] - change_state['RBdeg']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['LBdeg'] = String(change_state['LBdeg']['start'] + (emotion_preset[etype]['LBdeg'] - change_state['LBdeg']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['RBdeg'] = String(change_state['RBdeg']['start'] + (emotion_preset[etype]['RBdeg'] - change_state['RBdeg']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
 
-    DL.innerHTML = String(change_state['DL']['start'] + (emotion_preset[etype]['DL'] - change_state['DL']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
-    DR.innerHTML = String(change_state['DR']['start'] + (emotion_preset[etype]['DR'] - change_state['DR']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['DL'] = String(change_state['DL']['start'] + (emotion_preset[etype]['DL'] - change_state['DL']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['DR'] = String(change_state['DR']['start'] + (emotion_preset[etype]['DR'] - change_state['DR']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
     
-    LTBrow.innerHTML = String(change_state['LTBrow']['start'] + (emotion_preset[etype]['LTBrow'] - change_state['LTBrow']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
-    RTBrow.innerHTML = String(change_state['RTBrow']['start'] + (emotion_preset[etype]['RTBrow'] - change_state['RTBrow']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
-    LBBrow.innerHTML = String(change_state['LBBrow']['start'] + (emotion_preset[etype]['LBBrow'] - change_state['LBBrow']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
-    RBBrow.innerHTML = String(change_state['RBBrow']['start'] + (emotion_preset[etype]['RBBrow'] - change_state['RBBrow']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['LTBrow'] = String(change_state['LTBrow']['start'] + (emotion_preset[etype]['LTBrow'] - change_state['LTBrow']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['RTBrow'] = String(change_state['RTBrow']['start'] + (emotion_preset[etype]['RTBrow'] - change_state['RTBrow']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['LBBrow'] = String(change_state['LBBrow']['start'] + (emotion_preset[etype]['LBBrow'] - change_state['LBBrow']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['RBBrow'] = String(change_state['RBBrow']['start'] + (emotion_preset[etype]['RBBrow'] - change_state['RBBrow']['start']) * Math.sin(Math.PI * change_state['t'] / 2));
 
-    EyeDist.innerHTML = String(change_state['EyeDir']['start'][0] + (emotion_preset[etype]['EyeDir'][0] - change_state['EyeDir']['start'][0]) * Math.sin(Math.PI * change_state['t'] / 2));
-    EyeAngle.innerHTML = String(change_state['EyeDir']['start'][1] + (emotion_preset[etype]['EyeDir'][1] - change_state['EyeDir']['start'][1]) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['EyeDir'][0] = String(change_state['EyeDir']['start'][0] + (emotion_preset[etype]['EyeDir'][0] - change_state['EyeDir']['start'][0]) * Math.sin(Math.PI * change_state['t'] / 2));
+    face_state['EyeDir'][1] = String(change_state['EyeDir']['start'][1] + (emotion_preset[etype]['EyeDir'][1] - change_state['EyeDir']['start'][1]) * Math.sin(Math.PI * change_state['t'] / 2));
 
     // console.log(change_state['t']) // DEBUG to check the machine error
     if (Math.abs(change_state['t'] - 1) < change_state['thresh']) {
-        console.log("Animated Done");
+        // console.log("Animated Done");
         change_state['emotion'] = 'none';
         change_state['t'] = 0;
         change_state['changing'] = false;
@@ -451,14 +363,14 @@ function emotion_update() {
 }
 
 function drawLeftEye() {
-    let edist = parseFloat(EyeDist.innerHTML) * ratio;
-    let eangle = parseFloat(EyeAngle.innerHTML) * Math.PI/180;
+    let edist = face_state['EyeDir'][0] * ratio;
+    let eangle = face_state['EyeDir'][1] * Math.PI/180;
     
     let posX0 = 340 * ratio;
     let posY0 = 375 * ratio;
 
-    let hT = parseFloat(LTBrow.innerHTML) * (35*ratio - eyebrow_height/2)/100 + eyebrow_height/2; // eye's y radii = 35*ratio
-    let hB = parseFloat(LBBrow.innerHTML) * (35*ratio - eyebrow_height/2)/100 + eyebrow_height/2;
+    let hT = face_state['LTBrow'] * (35*ratio - eyebrow_height/2)/100 + eyebrow_height/2; // eye's y radii = 35*ratio
+    let hB = face_state['LBBrow'] * (35*ratio - eyebrow_height/2)/100 + eyebrow_height/2;
     let heightT = eyebrow_height; // height of the top eyebrow
     let heightB = eyebrow_height; // height of the bottom eyebrow
     let widthT = hT > 35*ratio/2 ? 0 : eyebrow_width*(eyebrow_height/2 - hT)/(35*ratio - eyebrow_height) + eyebrow_width;
@@ -481,14 +393,14 @@ function drawLeftEye() {
 }
 
 function drawRightEye() {
-    let edist = parseFloat(EyeDist.innerHTML) * ratio;
-    let eangle = parseFloat(EyeAngle.innerHTML) * Math.PI/180;
+    let edist = face_state['EyeDir'][0] * ratio;
+    let eangle = face_state['EyeDir'][1] * Math.PI/180;
 
     let posX0 = 460 * ratio;
     let posY0 = 375 * ratio;
 
-    let hT = parseFloat(RTBrow.innerHTML) * (35*ratio - eyebrow_height/2)/100 + eyebrow_height/2; // eye's y radii = 35*ratio
-    let hB = parseFloat(RBBrow.innerHTML) * (35*ratio - eyebrow_height/2)/100 + eyebrow_height/2;
+    let hT = face_state['RTBrow'] * (35*ratio - eyebrow_height/2)/100 + eyebrow_height/2; // eye's y radii = 35*ratio
+    let hB = face_state['RBBrow'] * (35*ratio - eyebrow_height/2)/100 + eyebrow_height/2;
     let heightT = eyebrow_height; // height of the top eyebrow
     let heightB = eyebrow_height; // height of the bottom eyebrow
     let widthT = hT > 35*ratio/2 ? 0 : eyebrow_width*(eyebrow_height/2 - hT)/(35*ratio - eyebrow_height) + eyebrow_width;
@@ -543,12 +455,12 @@ function drawEyeLight(posX, posY) {
 
 function drawLips() {
     // additional conditions may be further needed (left < right)
-    let thetaLB = parseFloat(LBdeg.innerHTML) * Math.PI/180;
-    let thetaRB = parseFloat(RBdeg.innerHTML) * Math.PI/180;
+    let thetaLB = face_state['LBdeg'] * Math.PI/180;
+    let thetaRB = face_state['RBdeg'] * Math.PI/180;
     let k = 0.2; // may be further controlled manually or automatically...
 
-    let offLT = [parseFloat(LTx.innerHTML), parseFloat(LTy.innerHTML)]; // [offsetX, offsetY]
-    let offRT = [parseFloat(RTx.innerHTML), parseFloat(RTy.innerHTML)];
+    let offLT = [face_state['LT'][0], face_state['LT'][1]]; // [offsetX, offsetY]
+    let offRT = [face_state['RT'][0], face_state['RT'][1]];
 
     let LT_x_coord = (300 + offLT[0]) * ratio;
     let LT_y_coord = (450 + offLT[1]) * ratio;
@@ -567,8 +479,8 @@ function drawLips() {
     let thetaLB_delta = Math.PI/2 - thetaLB;
     let thetaRB_delta = Math.PI/2 - thetaRB;
 
-    let dthetaLB = thetaLB_delta * parseFloat(DL.innerHTML) / 100;
-    let dthetaRB = thetaRB_delta * parseFloat(DR.innerHTML) / 100;
+    let dthetaLB = thetaLB_delta * face_state['DL'] / 100;
+    let dthetaRB = thetaRB_delta * face_state['DR'] / 100;
 
     let DLB_x_coord = LT_x_coord + maxDL*(1-(1-k)*Math.cos(thetaLB + dthetaLB)) * Math.cos(thetaLB + dthetaLB) * ratio;
     let DLB_y_coord = LT_y_coord + maxDL*(1-(1-k)*Math.cos(thetaLB + dthetaLB)) * Math.sin(thetaLB + dthetaLB) * ratio;
@@ -596,57 +508,3 @@ function drawLips() {
     ctx.lineWidth = 6 * ratio;
     ctx.stroke();
 }
-
-// function drawPie(sliceCount) {
-//   let size = 2 * Math.PI / sliceCount
-
-//   let angle = 0
-//   let n = 0
-//   while(n < sliceCount) {
-//     drawQuadrant(angle, angle + size, state.circle, state.colors[n])
-//     n++
-//     angle += size
-//   }
-
-//   function drawQuadrant(fromAngle, toAngle, circle, color) {
-//     let { pos: {x, y}, radius } = circle
-  
-//     ctx.beginPath() 
-//     ctx.fillStyle = color
-//     ctx.moveTo(x, y)
-//     ctx.arc(x, y, radius, fromAngle, toAngle)
-//     ctx.lineTo(x,y)
-//     ctx.fill()
-//   }
-  
-// }
-
-
-// function randomColor() {
-//   let r = randInt(0, 255)
-//   let g = randInt(0, 255)
-//   let b = randInt(0, 255)
-//   return `rgb(${r},${g},${b})`
-// }
-
-// function randInt(from, to) {
-//   let n = Math.floor(Math.random() * (to - from) + from)
-//   return n
-// }
-
-// function rotateSquareAndDrawSquare() {
-//   let { pos: { x, y } } = state.square
-//   ctx.save()
-//   ctx.translate(x, y)
-//   state.square.angle += 2
-//   let radians = state.square.angle * Math.PI / 180
-//   ctx.rotate(radians)
-//   drawSquare()
-//   ctx.restore()
-// }
-
-// function drawSquare() {
-//   let { side, color} = state.square
-//   ctx.fillStyle = color
-//   ctx.fillRect(-(side / 2), -(side / 2), side, side)
-// }
